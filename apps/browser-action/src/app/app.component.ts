99262cc533706fd6e5583a4a728c83cb22f8bba7
Aaron Brewer <spaceribs@gmail.com>
2020-03-23 09:40:42 -0400

    migrate the majority of the browser-action

--- apps/browser-action/src/app/app.component.ts
+++ apps/browser-action/src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { Track } from '@plopdown/tracks';
+import { VideoRef, VideoElementRef } from '@plopdown/video-refs';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'plopdown-root',
+  selector: 'plopdown-browser-action',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'browser-action';
+  public loading = false;
+  public videoRefs: VideoRef[];
+  public tracks: Track[];
+  public foundVideos: VideoElementRef[];
+  public selectedVideo: VideoElementRef;
+  public selectedTrack: Track;
+  public openExtensionsPage(path: string) {}
+  public onSelectTrack() {}
+  public onRemoveVideo(videoRef: VideoRef) {}
+  public getVideoTitle(videoRef: VideoRef) {}
+  public queryVideos() {}
 }
