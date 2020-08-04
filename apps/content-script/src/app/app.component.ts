459d59cd0ef0e26267786424890129af10bfb0af
David Gage <whytheplatypus@gmail.com>
2020-08-04 15:49:05 -0400

    Load track from url first itteration
    - Changes are made as far forward as possible to make them obvious
    - track is lz-string compressed and uri safe base64 encoded
    
    Possible future simplifications
    - Drop Saved<Track><VideoRef> in favor of their base implementations
      (Track and VideoRef)
    	- the aspects added by the Saved<> interfaces aren't being from
    	  this point up.
    - URILoaderService or SubService to mimic BackgroundSubService
    	- A common interface for Track loading services

--- apps/content-script/src/app/app.component.ts
+++ apps/content-script/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { SavedVideoRef } from '@plopdown/video-refs';
+import { Track, SavedTrack } from '@plopdown/tracks';
+import { PlopdownFileService, PlopdownFile } from '@plopdown/plopdown-file';
 import { LoggerService } from '@plopdown/logger';
 import {
   ContentScriptPubService,
@@ -10,7 +12,8 @@ import {
   AfterViewInit,
 } from '@angular/core';
 import { map, scan } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, merge } from 'rxjs';
+import LZString from 'lz-string'
 
 @Component({
   selector: 'plopdown-cs',
@@ -27,10 +30,63 @@ export class AppComponent implements AfterViewInit {
 
   constructor(
     private bgSub: BackgroundSubService,
-    private logger: LoggerService
+    private logger: LoggerService,
+    private fileService: PlopdownFileService,
   ) {
-    this.videoRefs$ = this.bgSub.getVideoRefsFound().pipe(
+
+    const linkVideoRefs = new Observable(subscriber => {
+      if (window.location.hash.startsWith("#plopdown:")) {
+        const linkedVideo = LZString.decompressFromEncodedURIComponent(window.location.hash.split(":")[1]);
+        subscriber.next(linkedVideo);
+      }
+      subscriber.complete();
+    })
+
+    function convertVideoRef(file: PlopdownFile): [SavedVideoRef] {
+      const s: SavedTrack = {
+        _id: "local",
+        _rev: "local",
+        title: file.headers.title,
+        for: file.headers.for,
+        created: file.headers.created,
+        thumbnail: file.headers.thumbnail,
+        authors: file.headers.authors,
+        language: file.headers.language,
+        license: file.headers.license,
+        cues: file.cues,
+      };
+
+      const v: SavedVideoRef = {
+        _id: "local",
+        _rev: "local",
+        xpath: file.headers.xpath,
+        title: file.headers.for,
+        duration: 0,
+        frameOrigin: "",
+        track: s,
+        frameTitle: null,
+        framePath: null,
+        frameSearch: null,
+      }
+      return [v]
+    }
+
+    function importVTT(input: string): PlopdownFile {
+      try {
+        const f: PlopdownFile = fileService.decode(input);
+        return f
+      } catch(e) {
+        console.error(e);
+        throw e;
+      }
+    }
+
+    this.videoRefs$ = merge(linkVideoRefs.pipe(
+      map(importVTT),
+      map(convertVideoRef),
+    ), this.bgSub.getVideoRefsFound().pipe(
       map((res) => res.args[0]),
+    )).pipe(
       scan((refs, videoRefs) => {
         videoRefs.forEach((videoRef) => {
           refs.set(videoRef['_id'], videoRef);
