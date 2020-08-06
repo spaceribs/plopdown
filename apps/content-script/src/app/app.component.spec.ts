23996f27fe74827fcb740929a043ab5559c8dd4f
David Gage <whytheplatypus@gmail.com>
2020-08-06 14:07:07 -0400

    Read plopdown from hash on hashchange
    - trigger change detection on observable event
    - plopdown hash parsing is contained in window-ref
    - importVTT remains a local function to enable fileService to function
      (otherwise fileService.decode was a noop)

--- apps/content-script/src/app/app.component.spec.ts
+++ apps/content-script/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { MockLoggerModule } from '@plopdown/logger/mock';
 import { MockMessagesModule } from '@plopdown/messages/mock';
 import { MockPlopdownFileModule } from '@plopdown/plopdown-file/mock';
+import { MockWindowRefModule } from '@plopdown/window-ref/mock';
 import { MockVideoAttachmentsComponent } from '@plopdown/plopdown-injector/mock';
 import { MockContentScannerComponent } from './../../mock/content-scanner.component.mock';
 import { TestBed, async } from '@angular/core/testing';
@@ -9,7 +10,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MockLoggerModule, MockMessagesModule, MockPlopdownFileModule],
+      imports: [MockLoggerModule, MockMessagesModule, MockPlopdownFileModule, MockWindowRefModule],
       declarations: [
         AppComponent,
         MockContentScannerComponent,
