a5b62fe64387a50908070805722a1050f20ea6a3
Aaron Brewer <spaceribs@gmail.com>
2021-11-20 15:23:44 -0500

    get dragging working

--- libs/track-editor/src/lib/element/element.component.ts
+++ libs/track-editor/src/lib/element/element.component.ts
@@ -1,20 +1,48 @@
 import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
   Component,
   ElementRef,
+  EventEmitter,
   HostBinding,
   HostListener,
   Input,
+  NgZone,
+  OnDestroy,
+  Output,
+  Renderer2,
+  ViewChild,
 } from '@angular/core';
+import { animationFrameScheduler, fromEvent, Subscription } from 'rxjs';
+import {
+  map,
+  switchMap,
+  takeUntil,
+  throttleTime,
+  pluck,
+  filter,
+  mapTo,
+  sample,
+  distinctUntilChanged,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'plopdown-element',
   templateUrl: './element.component.html',
   styleUrls: ['./element.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ElementComponent {
+export class ElementComponent implements AfterViewInit, OnDestroy {
+  private subs: Subscription = new Subscription();
+
+  @Input() public zoom: number = 0;
   @Input() public totalTime: Date = new Date(0);
+
   @Input() public start: Date = new Date(0);
+  @Output() public startChange: EventEmitter<Date> = new EventEmitter();
+
   @Input() public end: Date = new Date(0);
+  @Output() public endChange: EventEmitter<Date> = new EventEmitter();
 
   @HostBinding('attr.tabindex') public tabIndex = 0;
 
@@ -22,19 +50,174 @@ export class ElementComponent {
   @HostBinding('style.background-color')
   public color: string = '#0F0';
 
-  @HostBinding('style.left.%')
+  @HostBinding('style.left.px')
   public get left(): number {
-    return (this.start.getTime() / this.totalTime.getTime()) * 100;
+    return this.start.getTime() / this.zoom;
   }
 
-  @HostBinding('style.right.%')
+  @HostBinding('style.right.px')
   public get right(): number {
-    return 100 - (this.end.getTime() / this.totalTime.getTime()) * 100;
+    return (this.totalTime.getTime() - this.end.getTime()) / this.zoom;
   }
 
   @HostListener('click', ['$event'])
   public onClick($event: MouseEvent) {
     $event.stopPropagation();
-    return false;
   }
+
+  @ViewChild('dragLeft')
+  private dragLeft: ElementRef<HTMLAnchorElement> | null = null;
+
+  @ViewChild('dragRight')
+  private dragRight: ElementRef<HTMLAnchorElement> | null = null;
+
+  public ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+
+  constructor(
+    private renderer: Renderer2,
+    private element: ElementRef,
+    private zone: NgZone
+  ) {}
+
+  public ngAfterViewInit(): void {
+    if (this.dragLeft == null || this.dragRight == null) {
+      return;
+    }
+
+    const dragLeftElem = this.dragLeft.nativeElement;
+    const dragRightElem = this.dragRight.nativeElement;
+
+    this.zone.runOutsideAngular(() => {
+      const drag$ = fromEvent<MouseEvent>(document, 'mousemove').pipe(
+        pluck<MouseEvent, number>('clientX'),
+        throttleTime(15, animationFrameScheduler, { trailing: true }),
+        distinctUntilChanged(),
+        filter((clientX) => clientX > 0)
+      );
+
+      const dragLeftStart$ = fromEvent<MouseEvent>(
+        dragLeftElem,
+        'mousedown'
+      ).pipe(pluck<MouseEvent, number>('clientX'));
+
+      const dragRightStart$ = fromEvent<MouseEvent>(
+        dragRightElem,
+        'mousedown'
+      ).pipe(pluck<MouseEvent, number>('clientX'));
+
+      const dragEnd$ = fromEvent(document, 'mouseup').pipe(mapTo(null));
+
+      const dragLeftMove$ = dragLeftStart$.pipe(
+        switchMap((startX) => {
+          return drag$.pipe(
+            map((dragX) => {
+              let dragPos = dragX - startX;
+              return dragPos;
+            }),
+            takeUntil(dragEnd$)
+          );
+        })
+      );
+
+      const dragRightMove$ = dragRightStart$.pipe(
+        switchMap((startX) => {
+          return drag$.pipe(
+            map((dragX) => {
+              let dragPos = startX - dragX;
+              return dragPos;
+            }),
+            takeUntil(dragEnd$)
+          );
+        })
+      );
+
+      const finalLeftPos$ = dragLeftMove$.pipe(
+        sample(dragEnd$),
+        map((offset) => {
+          return new Date(this.start.getTime() + offset * this.zoom);
+        })
+      );
+      const finalRightPos$ = dragRightMove$.pipe(
+        sample(dragEnd$),
+        map((offset) => {
+          return new Date(this.end.getTime() - offset * this.zoom);
+        })
+      );
+
+      const dragLeftSub = dragLeftMove$.subscribe((offset) => {
+        this.renderer.setStyle(
+          this.element.nativeElement,
+          'margin-left',
+          `${offset}px`
+        );
+      });
+      this.subs.add(dragLeftSub);
+
+      const dragRightSub = dragRightMove$.subscribe((offset) => {
+        this.renderer.setStyle(
+          this.element.nativeElement,
+          'margin-right',
+          `${offset}px`
+        );
+      });
+      this.subs.add(dragRightSub);
+
+      const finalLeftSub = finalLeftPos$.subscribe((finalLeft) => {
+        this.renderer.setStyle(this.element.nativeElement, 'margin-left', null);
+        this.startChange.emit(new Date(finalLeft));
+      });
+      this.subs.add(finalLeftSub);
+
+      const finalRightSub = finalRightPos$.subscribe((finalRight) => {
+        this.renderer.removeStyle(this.element.nativeElement, 'margin-right');
+        this.endChange.emit(new Date(finalRight));
+      });
+      this.subs.add(finalRightSub);
+    });
+  }
+
+  public dragLeftStart($event: MouseEvent) {
+    // if (this.dragLeft == null) {
+    //   return;
+    // }
+    // const draggableElement = this.dragLeft.nativeElement;
+    // const mouseDown$ = fromEvent<MouseEvent>(draggableElement, 'mousedown');
+    // const mouseMove$ = fromEvent<MouseEvent>(draggableElement, 'mousemove');
+    // const mouseUp$ = fromEvent<MouseEvent>(draggableElement, 'mouseup');
+    // const mouseLeave$ = fromEvent<MouseEvent>(draggableElement, 'mouseleave');
+    // const dragMove$ = mouseDown$.pipe(
+    //   switchMap((start) =>
+    //     mouseMove$.pipe(
+    //       // we transform the mouseDown and mouseMove event to get the necessary information
+    //       map((moveEvent) => ({
+    //         originalEvent: moveEvent,
+    //         deltaX: moveEvent.pageX - start.pageX,
+    //         deltaY: moveEvent.pageY - start.pageY,
+    //         startOffsetX: start.offsetX,
+    //         startOffsetY: start.offsetY,
+    //       })),
+    //       takeUntil(mouseUp$),
+    //       takeUntil(mouseLeave$)
+    //     )
+    //   )
+    // );
+    // dragMove$.subscribe((move) => {
+    //   const offsetX = move.originalEvent.x - move.startOffsetX;
+    //   const offsetY = move.originalEvent.y - move.startOffsetY;
+    //   // draggableElement.style.left = offsetX + 'px';
+    //   // draggableElement.style.top = offsetY + 'px';
+    //   console.log(offsetX, offsetY);
+    // });
+  }
+
+  public dragLeftEnd() {}
+
+  public dragRightStart($event: MouseEvent) {
+    // console.log(this.end);
+    // this.endChange.emit(new Date(0));
+  }
+
+  public dragRightEnd() {}
 }
